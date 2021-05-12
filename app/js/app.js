3f0f2aa3068c57759d2fcb9ad83432df432c9caa
NHEANEY99 <78561135+NHEANEY99@users.noreply.github.com>
2021-05-12 16:53:07 -0400

    Edited files to fix console errors

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,14 @@
 window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
+
+       // let sign = prompt("Enter a number:");
+        const aValue = Number(window.prompt("Enter a number", ""));
+
+    for (let i = 1; i < 30; i++) {
+        for (let j = 1; j < i; j++) {
+            console.log("*");
+        }
+        console.log("\n");
+    }
 };
